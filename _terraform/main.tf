26382c4bc436bf7f1b610c906c34181bf570d24f
Seth Swanson <sethswanson@fastmail.com>
2023-03-30 10:40:42 -0600

    Resolved merge conflict

--- _terraform/main.tf
+++ _terraform/main.tf
@@ -4,3 +4,20 @@ resource "tls_private_key" "test" {
   algorithm = "RSA"
   rsa_bits  = "4096"
 }
+
+resource "aws_security_group" "snyk_tst" {
+  name = "snyk_tst"
+  description = "Triggering snyk"
+
+  ingress = [ {
+    cidr_blocks = []
+    description = "EVERYTHING"
+    from_port = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocol = -1
+    security_groups = []
+    self = false
+    to_port = 0
+  } ]
+}
