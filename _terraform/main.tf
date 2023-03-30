9758c0e9f593e4dacb3dd9d2f0793a9a4e56e208
Seth Swanson <sethswanson@fastmail.com>
2023-03-30 09:51:38 -0600

    Added wide opn SG to trigger vuln

--- _terraform/main.tf
+++ _terraform/main.tf
@@ -1,1 +1,18 @@
 data "aws_caller_identity" "current" {}
+
+resource "aws_security_group" "snyk_tst" {
+  name = "snyk_tst"
+  description = "Triggering snyk"
+
+  ingress = [ {
+    cidr_blocks = []
+    description = "EVERYTHING"
+    from_port = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocol = -1
+    security_groups = []
+    self = false
+    to_port = 0
+  } ]
+}
